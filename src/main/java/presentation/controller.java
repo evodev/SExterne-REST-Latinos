88f1ec0159d1bdd400f8133a6352e98bc127bebf
Manuel <manuelferreira4@outlook.pt>
2023-04-24 11:45:13 +0200

    delete unused imports and methods from controller.java

--- src/main/java/presentation/controller.java
+++ src/main/java/presentation/controller.java
@@ -4,25 +4,13 @@ import business.ActeAuthentique;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class controller {
-
-  /*@PostMapping("/archive")
-  public ResponseEntity<String> archiveJson(@RequestBody String json) {
-    // Traiter le JSON ici
-    System.out.println("JSON reçu : " + json);
-
-    // Archiver le JSON
-
-    return new ResponseEntity<>("JSON archivé avec succès", HttpStatus.OK);
-  }*/
-
-  @PostMapping("/archive")
+    @PostMapping("/archive")
   public ResponseEntity<String> archiveJson(@RequestBody String json) {
     try {
       // Désérialiser le JSON en une instance de la classe ActeAuthentique
